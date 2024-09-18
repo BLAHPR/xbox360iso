c6c525da30dd60e7257c5610dafca830d3cf3abc
Blahpr <tendonen7@gmail.com>
2024-09-17 20:54:17 -0700

    Add files via upload

--- build script.bat
+++ build script.bat
@@ -1,3 +1,3 @@
-pyinstaller --onefile --add-data "xbox360_gamelist.csv;." --name "xbox360iso" xbox360iso.py
+pyinstaller --onefile --add-data "xbox360_gamelist.csv;." --upx-dir "C:\upx-4.2.4-win64" --name "xbox360iso" xbox360iso.py
 
 pause
